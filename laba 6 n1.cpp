c20f4eebc52fa5d3e6e8f189d8a2b1ee56ce719b
Zahar888 <60703305+Zahar888@users.noreply.github.com>
2024-12-23 12:17:38 +0200

    Add files via upload

---
+++ laba 6 n1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    const int SIZE = 20; 
+    int arr[SIZE];       
+    int numberToFind;    
+    int index = -1;      
+
+    
+    cout << "Enter 20 numbers: ";
+    for (int i = 0; i < SIZE; i++) {
+        cin >> arr[i]; 
+    }
+
+    
+    cout << "Enter a number to search: ";
+    cin >> numberToFind;
+
+    for (int i = 0; i < SIZE; i++) {
+        if (arr[i] == numberToFind) {
+            index = i; 
+            break;     
+        }
+    }
+    if (index != -1) {
+        cout << "Number found on index: " << index << endl;
+    }
+    else {
+        cout << "Such a number has not been found in the array." << endl;
+    }
+
+    return 0;
+}
---
+++ laba 6 n2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int size;
+    double* arr;
+    double max = 0;
+    cout << "Enter size of array: ";
+    cin >> size;
+    arr = new double[size];
+    cout << "Enter " << size << " numbers: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = arr[i] / max;
+    }
+
+    cout << "Normalized array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
